9f2ef845df8f7903f860a5f6e6e18d0f960a3472
Awlter <wxfwalter@gmail.com>
2018-03-18 23:09:29 +0800

    create an API client

--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -5,9 +5,16 @@ module Reports
 
   class GitHubAPIClient
     User = Struct.new(:name, :location, :public_repos)
-    
+
     def self.get_user(username)
-      response = Faraday.get "https://api.github.com/users/#{username}"
+      url = "https://api.github.com/users/#{username}"
+
+      start_time = Time.now
+      response = Faraday.get url
+      duration = Time.now - start_time
+
+      puts "-> %s %s %d" % [url, 'GET', response.status]
+
       data = JSON.parse(response.body)
       User.new(data['name'], data['location'], data['public_repos'])
     end
