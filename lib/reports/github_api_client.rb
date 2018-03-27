a7bd30f0c9ba6ab32be0f8dd10cc04339493e1a7
Awlter <wxfwalter@gmail.com>
2018-03-27 15:23:14 +0800

    handle invalid service response code

--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -4,9 +4,12 @@ require 'logger'
 require 'byebug'
 
 module Reports
-  class NonexistentUser < StandardError; end
+  class Error < StandardError; end
+  class RequestFailure < Error; end
+  class NonexistentUser < Error; end
 
   User = Struct.new(:name, :location, :public_repos)
+  VALID_STATUS_CODES = [200, 302, 403, 404, 422]
 
   class GitHubAPIClient
     attr_reader :logger
@@ -24,6 +27,10 @@ module Reports
       duration = Time.now - start_time
 
       logger.debug "-> %s %s %d (%.3f s)" % [url, 'GET', response.status, duration]
+
+      if !VALID_STATUS_CODES.include?(response.status)
+        raise RequestFailure, JSON.parse(response.body)['message']
+      end
 
       if response.status == 404
         raise NonexistentUser, "'#{username}' doesn't exist."
